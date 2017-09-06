5f21c0a71c8695ca45e80504ae75eed83a298345
Raymond Gregory <vinedsel@gmail.com>
2017-09-05 21:46:33 -0500

    modded files

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-$('#fullname').focus(function(){
+$('#fullname').focus(function() {
   let tag = $('input');
   $(this).addClass('Highlight');
 });
+
+
+// $('#fullname').click(function() { alert('The element myID was clicked'); });
