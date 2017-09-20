b674ed5e90494b7fff8f1d0a6c47f11919a0b068
Raymond Gregory <vinedsel@gmail.com>
2017-09-19 23:22:20 -0500

    FINALLY got mouseover events to work when hovering

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-$('#fullname').focus(function() {
-  let tag = $('input');
-  $(this).addClass('Highlight');
-});
-
+// $(document).ready(function() {
+//   $('#fullname').focus(function() {
+//     $(this).addClass('highlight');
+//   });
+// });
 
 // $('#fullname').click(function() { alert('The element myID was clicked'); });
+
+$(document).ready(function() {
+  $("#fullname").mouseover(function() {
+    $(this).addClass("focused");
+  });
+  $("#fullname").mouseout(function() {
+    $(this).removeClass("focused");
+  });
+});
