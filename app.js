8969ed1045ad5ec4b65068764902e63edcdf87f6
Raymond Gregory <vinedsel@gmail.com>
2017-11-15 04:28:43 -0600

    implemented bootstrap grid system site-wide, fixed other minor annoyances. And implemented thumbnails.

--- app.js
+++ app.js
@@ -45,3 +45,32 @@ $(document).ready(function() {
       }
     }
   }
+
+
+    /* TODO: Move To js file */
+    /*********************************************************
+     *
+     *   Test Checkboxes/Radio Buttons Script
+     *
+     *********************************************************/
+    // loop through each form and handle submit event
+    for (var i = 0; i < document.forms.length; i++) {
+      var form = document.forms[i];
+      form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        var results = '';
+        // loop through all checkboxes to see if checked
+        var checkboxes = form.querySelectorAll("input[type='checkbox']");
+        for (var k = 0; k < checkboxes.length; k++) {
+          results += checkboxes[k].id + '=' + checkboxes[k].checked + '\n';
+        }
+        // loop through all radio butotns to display value
+        var radiobuttons = form.querySelectorAll("input[type='radio']:checked");
+        for (var m = 0; m < radiobuttons.length; m++) {
+          results += radiobuttons[m].name + '=' + radiobuttons[m].value + '\n';
+        }
+        // alert results
+        alert(results);
+      });
+    }
+  
