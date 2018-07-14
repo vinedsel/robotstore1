d251b0e35a914e1bd7d187c44b0d71ba1a479547
Raymond Gregory <vinedsel@gmail.com>
2018-07-13 21:46:55 -0500

    slight modifications

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
 
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("h4")[0];
+      a = li[i].getElementsByTagName("article")[0];
       if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
         li[i].style.display = "";
       } else {
